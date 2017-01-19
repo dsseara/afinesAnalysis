70912636d667b17a201e8d40200930f3fac61037
Danny Seara <daniel.seara@yale.edu>
2017-01-19 17:46:14 -0500

    show mean of div is zero

--- strainTimeSeries.m
+++ strainTimeSeries.m
@@ -1,12 +1,18 @@
+%%
 % Script for calculating the strain rate time series for Afines data
-%
+% This uses an external file "strain.m" which finds the strain rate tensor, eigenvectors
+% and eigenvalues for the given vector field. I'm including that file at the end, it can be found
+% on my github page.
 
 clear;
 close all;
 tic;
 
+str = ['Data contained in: ',pwd];
+fprintf(str)
+
 fname = 'interpedData.mat';
-myVars = {'adataVelInterped'};
+myVars = {'adataVelInterped','dr'};
 load(fname, myVars{:});
 [~, ~, ~, nFrames] = size(adataVelInterped);
 strainRateSeries = zeros(nFrames,1);
@@ -18,16 +24,32 @@ for frame = 1:size(adataVelInterped,4)
     [strainRateTensor, strainRateEvecs, strainRateEvals] = strain(interpedVX, interpedVY, dr);
     div = sum(sum(strainRateEvals,3),4);
     strainRateSeries(frame) = mean(mean(div));
-
-    % pcolor(xGrid,yGrid,div);
-    % shading interp;
-    % axis image;
-    % colorbar;
-    % caxis([-1 1])
-    % hold on;
-    % quiver(xGrid, yGrid, interpedVX, interpedVY,'k')
-    % pause(0.1)
-    % hold off
 end
 
+plot(strainRateSeries, 'bo')
 
+Max = max(strainRateSeries);
+Min = min(strainRateSeries);
+Mean = mean(strainRateSeries);
+Std = std(strainRateSeries);
+xlim([0,500])
+ylimit = max([abs(Max), abs(Min)])*1.2;
+ylim([-ylimit, ylimit])
+txt = ['max = ', num2str(Max), ', min = ', num2str(Min)];
+txt2 = ['Mean \pm std = ', num2str(Mean), '\pm', num2str(Std)];
+text(250, ylimit*0.9, txt);
+text(250, ylimit*0.8, txt2);
+title('Mean of $\vec{\nabla} \cdot \vec{v}$ over whole domain', 'Interpreter', 'latex')
+
+
+%% 
+%
+% Here we see how the mean divergence of the entire frame is always around zero. This arises due to
+% a combination of periodic boundary conditions and mass conservation. The mass continuity equation is given
+% by $\partial_t m + \vec{\nabla} \cdot \vec{p} = 0$, and since $\partial_t m = 0$ with periodic boundaries,
+% we are left with $\vec{\nabla} \cdot \vec{p} = m_{bead} \vec{\nabla} \cdot \vec{v} = 0$, showing that the
+% divergence of the velocity field must be zero for periodic boundary conditions.
+%
+% Below is the strain.m function
+% 
+% <include>strain.m</include>
