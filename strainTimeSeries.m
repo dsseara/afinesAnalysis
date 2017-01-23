4cda0c3df7a942add6d6d1760f4690566b9c2f17
Danny Seara <dsseara@gmail.com>
2017-01-23 11:02:12 -0500

    new strain analysis using displacement of each actin bead

--- strainTimeSeries.m
+++ strainTimeSeries.m
@@ -1,55 +1,55 @@
 %%
 % Script for calculating the strain rate time series for Afines data
 % This uses an external file "strain.m" which finds the strain rate tensor, eigenvectors
-% and eigenvalues for the given vector field. I'm including that file at the end, it can be found
-% on my github page.
+% and eigenvalues for the given vector field. 
 
 clear;
 close all;
 tic;
 
 str = ['Data contained in: ',pwd];
-fprintf(str)
+fprintf(str);
 
 fname = 'interpedData.mat';
-myVars = {'adataVelInterped','dr'};
+myVars = {'adataVelInterped','dr', 'xRange', 'yRange'};
 load(fname, myVars{:});
 [~, ~, ~, nFrames] = size(adataVelInterped);
 strainRateSeries = zeros(nFrames,1);
+seriesStats.std = [];
+seriesStats.mean = [];
 
-for frame = 1:size(adataVelInterped,4)
-    interpedVX = adataVelInterped(:,:,1,frame);
-    interpedVY = adataVelInterped(:,:,2,frame);
+halfSpaceSize = min(xRange(2)-xRange(1), yRange(2)-yRange(1));
+for i = 0:floor(halfSpaceSize)-2 % Want to leave at least some space in the middle..
+    interiorXRange = [xRange(1)+i, xRange(2)-i];
+    interiorYRange = [yRange(1)+i, yRange(2)-i];
+    figure()
+    for frame = 1:size(adataVelInterped,4)
+        interpedVX = adataVelInterped((i+1):(end-i), (i+1):(end-i), 1, frame);
+        interpedVY = adataVelInterped((i+1):(end-i), (i+1):(end-i), 2, frame);
 
-    [strainRateTensor, strainRateEvecs, strainRateEvals] = strain(interpedVX, interpedVY, dr);
-    div = sum(sum(strainRateEvals,3),4);
-    strainRateSeries(frame) = mean(mean(div));
+        [strainRateTensor, strainRateEvecs, strainRateEvals] = strain(interpedVX, interpedVY, dr);
+        div = sum(sum(strainRateEvals,3),4);
+        strainRateSeries(frame) = mean(mean(div));
+    end
+    seriesStats.std = [seriesStats.std std(strainRateSeries)];
+    seriesStats.mean = [seriesStats.mean mean(strainRateSeries)];
+    plot((1:10:2000)./4,strainRateSeries(1:10:end),'o')
+    xlabel('time (s)')
+    ylabel('$ \dot{\epsilon}(t) $', 'Interpreter', 'LaTex')
+    txt = ['Mean $\pm$ std = ', num2str(seriesStats.mean(end)),'$\pm$' num2str(seriesStats.std(end))];
+    text(250, 0.5*max(strainRateSeries), txt, 'Interpreter','LaTeX');
+    txt2 = ['X-range = ', num2str(interiorXRange(1)), '-', num2str(interiorXRange(2))];
+    txt3 = [', Y-range = ', num2str(interiorYRange(1)), '-', num2str(interiorYRange(2))];
+    title([txt2, txt3])
+
 end
 
-plot(strainRateSeries, 'bo')
-
-Max = max(strainRateSeries);
-Min = min(strainRateSeries);
-Mean = mean(strainRateSeries);
-Std = std(strainRateSeries);
-xlim([0,500])
-ylimit = max([abs(Max), abs(Min)])*1.2;
-ylim([-ylimit, ylimit])
-txt = ['max = ', num2str(Max), ', min = ', num2str(Min)];
-txt2 = ['Mean \pm std = ', num2str(Mean), '\pm', num2str(Std)];
-text(250, ylimit*0.9, txt);
-text(250, ylimit*0.8, txt2);
-title('Mean of $\vec{\nabla} \cdot \vec{v}$ over whole domain', 'Interpreter', 'latex')
-
+errorbar(0:(numel(seriesStats.mean)-1), seriesStats.mean, seriesStats.std, 'o');
+hold on;
+plot([0,30],[0,0])
+xlim([0,30])
+legend('Mean Strain Rate, \mu \pm \sigma', 'Zero Line')
 
 %% 
-%
-% Here we see how the mean divergence of the entire frame is always around zero. This arises due to
-% a combination of periodic boundary conditions and mass conservation. The mass continuity equation is given
-% by $\partial_t m + \vec{\nabla} \cdot \vec{p} = 0$, and since $\partial_t m = 0$ with periodic boundaries,
-% we are left with $\vec{\nabla} \cdot \vec{p} = m_{bead} \vec{\nabla} \cdot \vec{v} = 0$, showing that the
-% divergence of the velocity field must be zero for periodic boundary conditions.
-%
-% Below is the strain.m function
 % 
-% <include>strain.m</include>
+% These plots show the mean strain rate over time over an increasingly small area in the center of the domain of the simulation. As we can see, the average strain rate is consistently zero, despite the changing box size. Although there always does seem to be a peak at the very beginning of the simulation.
